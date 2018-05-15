8843175e8b43adaa8cb918fe6326fd99bcdfc899
HugoPlatzer <hugoplatzer@aon.at>
2018-05-15 12:07:45 +0200

    complete crop

--- code/crop/crop.py
+++ code/crop/crop.py
@@ -1,17 +1,17 @@
 import cv2 as cv
 import numpy as np
 
-def drawLines(img, lines):
-  for rho,theta in lines:
+def drawLines(img, lines, color = (0, 0, 255), size = 1):
+  for rho, theta in lines:
     a = np.cos(theta)
     b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 10000*(-b))
-    y1 = int(y0 + 10000*(a))
-    x2 = int(x0 - 10000*(-b))
-    y2 = int(y0 - 10000*(a))
-    cv.line(img,(x1,y1),(x2,y2),(0,0,255),1)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 100000*(-b))
+    y1 = int(y0 + 100000*(a))
+    x2 = int(x0 - 100000*(-b))
+    y2 = int(y0 - 100000*(a))
+    cv.line(img, (x1, y1), (x2, y2), color, size)
 
 def groupLines(lines):
   horiz = [l for l in lines if 0 <= l[1] <= 0.05 * np.pi or 0.95 * np.pi <= l[1] <= np.pi]
@@ -56,31 +56,42 @@ def cropPoints(img, groups):
   topRightPoint = max(groups[1], key = lambda p : np.linalg.norm(topRight - p))
   btmLeftPoint = max(groups[2], key = lambda p : np.linalg.norm(btmLeft - p))
   btmRightPoint = max(groups[3], key = lambda p : np.linalg.norm(btmRight - p))
-  topLeftPoint = (max(topLeftPoint[0], btmLeftPoint[0]), topLeftPoint[1])
-  btmRightPoint = (min(topRightPoint[0], btmRightPoint[0]), btmRightPoint[1])
-  return (topLeftPoint, btmRightPoint)
+  minX = max(topLeftPoint[0], btmLeftPoint[0])
+  maxX = min(topRightPoint[0], btmRightPoint[0])
+  minY = max(topLeftPoint[1], topRightPoint[1])
+  maxY = min(btmLeftPoint[1], btmRightPoint[1])
+  cropTopLeft = (minX, minY)
+  cropBtmRight = (maxX, maxY)
+  return (cropTopLeft, cropBtmRight)
 
 def drawPoints(img, points, color = (0, 255, 0), size = 2):
   for p in points:
     cv.line(img, p, p, color, size)
 
 np.seterr(all = "ignore")
-img = cv.imread("samples/sample1.jpg")
+img = cv.imread("/home/hugo/out0012.pnm")
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(gray, 100, 255, cv.THRESH_BINARY)
+gray = 255 - gray
+#~ gray = cv.GaussianBlur(gray, (3, 3), 0)
+
+gray = cv.equalizeHist(gray)
+cv.imshow("gray", gray)
+ret, thresh = cv.threshold(gray, 230, 255, cv.THRESH_BINARY)
+thresh = cv.medianBlur(thresh, 5)
+
 cv.imshow("thresh", thresh)
-edges = cv.Canny(thresh, 70, 150)
-#cv.imshow("edges", edges)
+edges = cv.Laplacian(thresh, 0)
 lines = cv.HoughLines(edges, 1, np.pi / 720, 150)
 lines = [l[0] for l in lines] if lines is not None else []
 horiz, verti = groupLines(lines)
 points = intersectLines(horiz, verti)
 corners = groupPoints(img, points)
-drawPoints(img, points)
 cropFrame = cropPoints(img, corners)
 print(cropFrame)
 cropImg = img.copy()[cropFrame[0][1]:cropFrame[1][1], cropFrame[0][0]:cropFrame[1][0]]
-cv.rectangle(img, cropFrame[0], cropFrame[1], (0, 0, 255), 1)
+drawLines(img, lines)
+drawPoints(img, points)
+cv.rectangle(img, cropFrame[0], cropFrame[1], (255, 0, 0), 1)
 cv.imshow("img", img)
-#cv.imshow("crop", cropImg)
+cv.imshow("crop", cropImg)
 cv.waitKey(0)
