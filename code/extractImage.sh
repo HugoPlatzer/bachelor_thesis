9151c393ad38b8b3b277ca4906d703e6108ac8ae
HugoPlatzer <hugoplatzer@aon.at>
2018-06-22 01:28:31 +0200

    added images again

---
+++ code/extractImage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PCAPFILE=../captures/scan_main.pcapng
+
+./extractImage.py -i $PCAPFILE -bo 300 -bn 2 -ll 1645 -lo 842 -ln 4 -lm 1060 -o out_r.png
+./extractImage.py -i $PCAPFILE -bo 300 -bn 2 -ll 1645 -lo 852 -ln 4 -lm 1060 -o out_g.png
+./extractImage.py -i $PCAPFILE -bo 300 -bn 2 -ll 1645 -lo 853 -ln 4 -lm 1060 -o out_i.png
+./extractImage.py -i $PCAPFILE -bo 300 -bn 2 -ll 1645 -lo 863 -ln 4 -lm 1060 -o out_b.png
+convert out_r.png out_g.png out_b.png -combine -gamma 2 out.png
